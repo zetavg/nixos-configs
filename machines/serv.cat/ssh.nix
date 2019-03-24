d5412bcd7af51dba7c01e8da7df06cf1952a81ab
zetavg <mail@zeta.vg>
2019-03-24 22:30:52 +0800

    let serv.cat close a dead ssh connection to prevent binding port occupation

--- machines/serv.cat/ssh.nix
+++ machines/serv.cat/ssh.nix
@@ -5,4 +5,11 @@
     ../../common/openssh.nix
     ../../common/mosh.nix
   ];
+
+  # Close the connection after 4 × 8 = 32 secs of unreachability
+  # to prevent dead connections occupying the port.
+  services.openssh.extraConfig = ''
+    ClientAliveInterval 4
+    ClientAliveCountMax 8
+  '';
 }
