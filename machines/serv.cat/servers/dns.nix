88c4f292de9d313c1eee24a3f23ad57d610ec5f9
zetavg <mail@zeta.vg>
2019-08-05 11:37:37 +0800

    serv.cat: add comments about disabling the dns server

--- machines/serv.cat/servers/dns.nix
+++ machines/serv.cat/servers/dns.nix
@@ -1,15 +1,16 @@
 { ... }:
 
 {
-  imports = [
-    ../../../common/bind.nix
-  ];
+  # This is currently not enabled since the DNS server of the domain serv.cat is Gandi.
+  # imports = [
+  #   ../../../common/bind.nix
+  # ];
 
-  services.bind.zones = [
-    {
-      name = "serv.cat";
-      master = true;
-      file = "/home/var/named/serv.cat";
-    }
-  ];
+  # services.bind.zones = [
+  #   {
+  #     name = "serv.cat";
+  #     master = true;
+  #     file = "/home/var/named/serv.cat";
+  #   }
+  # ];
 }
