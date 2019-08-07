6e65bba69445de1b518d2de281999775ef17820a
zetavg <mail@zeta.vg>
2019-08-07 12:23:59 +0800

    logstash: customize index template and update filter

--- common/elk.nix
+++ common/elk.nix
@@ -20,7 +20,82 @@
 
 { pkgs, lib, ... }:
 
-{
+let
+  logstashElasticsearchTemplateFile = pkgs.writeText "logstash-es-template.json" ''
+    {
+      "order" : 0,
+      "version" : 60001,
+      "index_patterns" : [
+        "logstash-*"
+      ],
+      "settings" : {
+        "index" : {
+          "lifecycle" : {
+            "name" : "logstash-policy",
+            "rollover_alias" : "logstash"
+          },
+          "number_of_shards" : "1",
+          "refresh_interval" : "5s"
+        }
+      },
+      "mappings" : {
+        "dynamic_templates" : [
+          {
+            "message_field" : {
+              "path_match" : "message",
+              "mapping" : {
+                "norms" : false,
+                "type" : "text"
+              },
+              "match_mapping_type" : "string"
+            }
+          }
+        ],
+        "properties" : {
+          "@timestamp" : {
+            "type" : "date"
+          },
+          "geoip" : {
+            "dynamic" : true,
+            "properties" : {
+              "ip" : {
+                "type" : "ip"
+              },
+              "latitude" : {
+                "type" : "half_float"
+              },
+              "location" : {
+                "type" : "geo_point"
+              },
+              "longitude" : {
+                "type" : "half_float"
+              }
+            }
+          },
+          "type" : {
+            "type" : "keyword"
+          },
+          "facility" : {
+            "type" : "keyword"
+          },
+          "ident" : {
+            "type" : "keyword"
+          },
+          "priority" : {
+            "type" : "keyword"
+          },
+          "method" : {
+            "type" : "keyword"
+          },
+          "@version" : {
+            "type" : "keyword"
+          }
+        }
+      },
+      "aliases" : { }
+    }
+  '';
+in {
   # The elasticsearch package has an unfree license
   nixpkgs.config.allowUnfree = true;
 
@@ -37,10 +112,10 @@
     '';
     filterConfig = ''
       if [type] == "syslog" {
+        # Use the systemd timestamp
         ruby {
           code => "event.set('timestamp_ms', event.get('__REALTIME_TIMESTAMP').to_i / 1000)"
         }
-
         date {
           match => ["timestamp_ms", "UNIX_MS"]
         }
@@ -56,6 +131,7 @@
           ]
         }
 
+        # Normalize field names
         mutate {
           rename => ["SYSLOG_FACILITY", "facility"]
           rename => ["SYSLOG_IDENTIFIER", "ident"]
@@ -67,6 +143,7 @@
           rename => ["MESSAGE", "message"]
         }
 
+        # Normalize priority values from systemd
         if [priority] == "0" {
           mutate { update => { "priority" => "emerg" } }
         } else if [priority] == "1" {
@@ -85,6 +162,10 @@
           mutate { update => { "priority" => "debug" } }
         }
 
+        # Normalize facility values from systemd
+        mutate {
+          lowercase => ["facility"]
+        }
         if [facility] == "0" {
           mutate { update => { "facility" => "kern" } }
         } else if [facility] == "1" {
@@ -134,11 +215,55 @@
         } else if [facility] == "23" {
           mutate { update => { "facility" => "local7" } }
         }
+
+        # Kibana specific operations
+        if [ident] == "kibana" {
+          json {
+            source => "message"
+            target => "kibana"
+          }
+          if [kibana] {
+            mutate {
+              rename => { "message" => "raw_message" }
+              add_field => { "message" => "[%{[kibana][type]}] %{[kibana][message]}" }
+            }
+          }
+        }
+
+        # Nginx specific operations
+        if [ident] == "nginx" {
+          mutate {
+            copy => { "message" => "raw_message" }
+          }
+          grok {
+            # match => {
+            #   "message" => "(?<nginx_timestamp>\d{4}/\d{2}/\d{2}\s{1,}\d{2}:\d{2}:\d{2})\s{1,}\[%{DATA:priority}\]\s{1,}(%{NUMBER:pid:int}#%{NUMBER:process_id}:\s{1,}\*%{NUMBER:thread_id}|\*%{NUMBER:connect_id}) %{DATA:message}(?:,\s{1,}client:\s{1,}(?<client_ip>%{IP}|%{HOSTNAME}))(?:,\s{1,}server:\s{1,}%{IPORHOST:server})(?:, request: %{QS:request})?(?:, host: %{QS:client_ip})?(?:, referrer: \"%{URI:referrer})?"
+            # }
+            match => {
+              "message" => "(?<nginx_timestamp>\d{4}/\d{2}/\d{2}\s{1,}\d{2}:\d{2}:\d{2})\s{1,}\[%{DATA:priority}\]\s{1,}%{GREEDYDATA:message}"
+            }
+            overwrite => ["priority", "message"]
+          }
+        }
+
+        # Normalize priority values
+        mutate {
+          lowercase => ["priority"]
+        }
+        if [priority] == "panic" {
+          mutate { update => { "priority" => "emerg" } }
+        } else if [priority] == "error" {
+          mutate { update => { "priority" => "err" } }
+        } else if [priority] == "warn" {
+          mutate { update => { "priority" => "warning" } }
+        }
       }
     '';
     outputConfig = ''
       elasticsearch {
         hosts => ["127.0.0.1:9200"]
+        template => "${logstashElasticsearchTemplateFile}"
+        template_overwrite => true
       }
     '';
   };
